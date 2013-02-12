dff755a8666be148186cb7ef233c7686c53f3b28
Valery Votintsev <vvotintsev@gmail.com>
2013-02-12 14:35:23 +0400

    Field ibf_session::browser length expanded to 255 chars

---
+++ db_schema/db/2013_02_12_14_25_50.php
@@ -0,0 +1,21 @@
+<?php
+
+class Migration_2013_02_12_14_25_50 extends MpmMigration {
+
+	public function up(PDO &$pdo) {
+		$pdo->query("
+		  ALTER TABLE `ibf_sessions` CHANGE `browser`
+		  `browser` VARCHAR( 255 ) NOT NULL DEFAULT ''
+		");
+		return TRUE;
+	}
+
+	public function down(PDO &$pdo) {
+		$pdo->query("
+		  ALTER TABLE `ibf_sessions` CHANGE `browser`
+		  `browser` VARCHAR( 64 ) NULL DEFAULT NULL
+		");
+		return TRUE;
+	}
+
+}
