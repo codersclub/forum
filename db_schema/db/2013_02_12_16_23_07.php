bf7ee6a10b42a712087694120785b83392477ce3
Valery Votintsev <vvotintsev@gmail.com>
2013-02-12 20:28:48 +0400

    Add "data" field into ibf_validating

---
+++ db_schema/db/2013_02_12_16_23_07.php
@@ -0,0 +1,23 @@
+<?php
+
+class Migration_2013_02_12_16_23_07 extends MpmMigration
+{
+
+	public function up(PDO &$pdo)
+	{
+		$pdo->query("
+		  ALTER TABLE `ibf_validating` ADD
+		  `data` TEXT NOT NULL DEFAULT '' AFTER `member_id`
+		");
+	}
+
+	public function down(PDO &$pdo)
+	{
+		$pdo->query("
+		  ALTER TABLE `ibf_validating` DROP `data`		
+		");
+	}
+
+}
+
+?>
