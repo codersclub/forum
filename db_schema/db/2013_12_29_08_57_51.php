b4d4d53d6d3427830e0de8c6e3ab140677f885cf
Mikhail Galanin <bespoleznyak@yandex.ru>
2013-12-29 13:01:01 +0400

    Index for slow query:
    SELECT
                            p.*,
                            s.id as s_id,
                            m.id, m.name, m.mgroup, m.email, m.joined,
                            m.gender,
                            m.avatar, m.avatar_size, m.posts, m.aim_name,
                            m.icq_number, m.signature,  m.website, m.yahoo,
                            m.integ_msg, m.title, m.hide_email, m.msnname,
                            m.warn_level, m.warn_lastwarn,
                            m.points,  m.fined, m.rep, m.ratting, m.show_ratting,
                            g.g_id, g.g_title, g.g_icon, g.g_use_signature,
                            g.g_use_avatar, g.g_dohtml
                            , pc.*
                      FROM ibf_posts p
                      LEFT JOIN ibf_members m
                            ON (p.author_id=m.id)
                      LEFT JOIN ibf_sessions s
                            ON (s.member_id != 0 and m.id=s.member_id and s.login_type != 1)
                      LEFT JOIN ibf_groups g
                            ON (g.g_id=m.mgroup)
                      LEFT JOIN ibf_pfields_content pc
                                           ON (pc.member_id=p.author_id)
                      WHERE p.topic_id='227346' GROUP BY p.pid ORDER BY p.pid LIMIT 165,15;

---
+++ db_schema/db/2013_12_29_08_57_51.php
@@ -0,0 +1,18 @@
+<?php
+
+class Migration_2013_12_29_08_57_51 extends MpmMigration
+{
+
+	public function up(PDO &$pdo)
+	{
+		$pdo->exec('ALTER TABLE ibf_posts ADD KEY topic_id_id_idx  (topic_id, pid)');
+	}
+
+	public function down(PDO &$pdo)
+	{
+		$pdo->exec('ALTER TABLE ibf_posts DROP KEY topic_id_id_idx;');
+	}
+
+}
+
+?>
