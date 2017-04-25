a54252746015135e532a16d7e34e4f0f7e4f2f47
Galanin Mikhail <mikhail.galanin@yahoo.com>
2017-04-25 22:15:41 +0100

    Added cmake and bcb

---
+++ db_schema/db/2017_04_25_19_32_25.php
@@ -0,0 +1,18 @@
+<?php
+
+class Migration_2017_04_25_19_32_25 extends MpmMigration
+{
+
+	public§ function up(PDO &$pdo)
+	{
+		$pdo->exec("alter table ibf_members modify `syntax` enum('client','server','none','prism-coy','prism-twilight') NOT NULL DEFAULT 'client';");
+	}
+
+	public function down(PDO &$pdo)
+	{
+		$pdo->exec("alter table ibf_members modify `syntax` enum('client','server','none') NOT NULL DEFAULT 'client';");
+	}
+
+}
+
+?>
