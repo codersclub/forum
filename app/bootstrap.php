6c1275391c438de12927db0afa686d96155b2007
Mikhail Galanin <mikhail.galanin@corp.badoo.com>
2017-05-03 20:05:57 +0100

    Https

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -36,3 +36,20 @@ if (!file_exists(__DIR__ . '/../conf_global.php')) {
     throw new Exception('Please create or copy "conf_global.php"');
 }
 require __DIR__ . '/../conf_global.php';
+
+$source_url = 'http://' . $_SERVER['HTTP_HOST'];
+$rewritten_url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'];
+
+if ($source_url != $rewritten_url) {
+    $url_params = [
+        'auto_pm_message',
+        'board_url',
+        'gl_link',
+        'html_url',
+        'upload_url'
+    ];
+
+    foreach ($url_params as $param) {
+        $INFO[$param] = str_replace($source_url, $rewritten_url, $INFO[$param]);
+    }
+}
