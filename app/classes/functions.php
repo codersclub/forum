c76a90853a983ad307d185101ef41c881006403d
Valery Votintsev <vvotintsev@gmail.com>
2020-07-29 22:18:58 +0300

    New debugging function "dump()"

--- app/classes/functions.php
+++ app/classes/functions.php
@@ -22,6 +22,28 @@
  */
 use Views\View;
 
+/**
+ * Show debug info
+ * @param $data
+ * @param string $name
+ */
+function dump($data, $name = '')
+{
+    $buf = var_export($data, true);
+
+    $buf = str_replace('\\r', '', $buf);
+    $buf = preg_replace('/\=\>\s*\n\s*array/s', '=> array', $buf);
+
+    echo '<pre>';
+
+    if ($name) {
+        echo $name, '=';
+    }
+
+    echo $buf;
+    echo "</pre>\n";
+}
+
 class functions
 {
 
