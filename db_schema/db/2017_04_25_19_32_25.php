54d5e5cddd0aaf6f3749593a96e588ffb1039736
Valery Votintsev <vvotintsev@gmail.com>
2022-03-18 11:58:14 +0300

    Fix a bug in migration 17

--- db_schema/db/2017_04_25_19_32_25.php
+++ db_schema/db/2017_04_25_19_32_25.php
@@ -3,7 +3,7 @@
 class Migration_2017_04_25_19_32_25 extends MpmMigration
 {
 
-	public§ function up(PDO &$pdo)
+	public function up(PDO &$pdo)
 	{
 		$pdo->exec("alter table ibf_members modify `syntax` enum('client','server','none','prism-coy','prism-twilight') NOT NULL DEFAULT 'client';");
 	}
